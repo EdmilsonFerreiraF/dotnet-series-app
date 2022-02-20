54bd3b043269e70cb471811a5520d19589f9a209
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-20 14:02:22 -0300

    📝 :pencil: [Minor] Serie class

---
+++ Edmilson.Series/Classes/Serie.cs
@@ -0,0 +1,10 @@
+namespace Edmilson.Series
+{
+    public class Serie : BaseEntity
+    {
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+    }
+}
