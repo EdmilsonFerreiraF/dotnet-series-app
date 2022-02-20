e64831e6fee8cda290cf35aab81b655f22e859ef
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-20 14:33:04 -0300

    🚀 :rocket: [Add] SerieRepository class

---
+++ Edmilson.Series/Classes/SerieRepository.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using Edmilson.Series.Interfaces;
+
+namespace Edmilson.Series
+{
+    public class SerieRepository : IRepository<Serie>
+    {
+        public void Update(int id, Serie entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Serie entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int NextId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie ReturnById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
